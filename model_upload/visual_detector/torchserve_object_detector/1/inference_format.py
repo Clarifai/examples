2d338f07810dbc7953bdf7caa7fb754653513763
David Eigen <deigen@clarifai.com>
2024-02-13 15:42:37 -0500

    update comment

--- model_upload/visual_detector/torchserve_object_detector/1/inference_format.py
+++ model_upload/visual_detector/torchserve_object_detector/1/inference_format.py
@@ -17,8 +17,7 @@ class TorchserveDataConverter:
     }]
 
   def from_torchserve_output(self, pred: List) -> Dict:
-    # parse the torchserve output to a list of dictionaries
-    # from: [[x1, y1, x2, y2, conf, class_idx, label_str], ...]
-    # to: [{'bbox': [x1, y1, x2, y2], 'score': conf, 'label': label}, ...]
+    # parse the torchserve output to inference model format for a single image output
+    # from: [x1, y1, x2, y2, conf, class_idx, label_str]
+    # to: {'bbox': [x1, y1, x2, y2], 'score': conf, 'label': label
     return {'bbox': pred[:4], 'score': pred[4], 'label': pred[5]}
-
