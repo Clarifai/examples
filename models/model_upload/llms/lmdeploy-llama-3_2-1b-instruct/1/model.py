e8ad5ac5851c1fb3c47b779e2012a3bc2433894e
phatvo9 <phat.vo@clarifai.com>
2024-12-12 14:18:42 +0000

    refact model code

--- models/model_upload/llms/lmdeploy-llama-3_2-1b-instruct/1/model.py
+++ models/model_upload/llms/lmdeploy-llama-3_2-1b-instruct/1/model.py
@@ -1,12 +1,28 @@
+from copy import deepcopy
+import json
 import os
 from typing import Iterator
 
 from clarifai.runners.models.model_runner import ModelRunner
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2
 from clarifai_grpc.grpc.api.status import status_code_pb2
+from clarifai_grpc.grpc.api.status import status_code_pb2, status_pb2
 from google.protobuf import json_format
+
 from lmdeploy import pipeline, GenerationConfig, TurbomindEngineConfig
 from transformers import AutoTokenizer
+
+  
+def get_inference_params(request) -> dict:
+  """Get the inference params from the request."""
+  inference_params = {}
+  if request.model.model_version.id != "":
+    output_info = request.model.model_version.output_info
+    output_info = json_format.MessageToDict(output_info, preserving_proto_field_name=True)
+    if "params" in output_info:
+      inference_params = output_info["params"]
+  return inference_params
+
 
 class MyRunner(ModelRunner):
   """A custom runner that loads the model and generates text using lmdeploy inference.
@@ -28,143 +44,81 @@ class MyRunner(ModelRunner):
     """This is the method that will be called when the runner is run. It takes in an input and
     returns an output.
     """
-
-    # TODO: Could cache the model and this conversion if the hash is the same.
-    model = request.model
-    output_info = None
-    if request.model.model_version.id != "":
-      output_info = json_format.MessageToDict(
-          model.model_version.output_info, preserving_proto_field_name=True)
+    prompts = [inp.data.text.raw for inp in request.inputs]
+    inference_params = get_inference_params(request)
+    temperature = inference_params.get("temperature", 0.7)
+    max_tokens = inference_params.get("max_tokens", 256)
+    top_p = inference_params.get("top_p", .9)
+    
+    messages = []
+    for prompt in prompts:
+      try:
+        prompt = json.loads(prompt)
+      except:
+        prompt = [{"role": "user", "content": prompt}]
+      finally:
+        messages.append(prompt)
+    
+    messages = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    gen_config = GenerationConfig(temperature=temperature,
+                                  max_new_tokens=max_tokens,
+                                  top_p=top_p)
+    list_generated_text = self.pipe(messages, gen_config=gen_config)
 
     outputs = []
-    # TODO: parallelize this over inputs in a single request.
-    for inp in request.inputs:
-      output = resources_pb2.Output()
+    for text in list_generated_text:
+      raw_text = text.text#.replace("<|start_header_id|>assistant<|end_header_id|>\n\n", "")
+      outputs.append(
+        resources_pb2.Output(
+          data=resources_pb2.Data(text=resources_pb2.Text(raw=raw_text)),
+          status=status_pb2.Status(code=status_code_pb2.SUCCESS)
+        )
+      )
 
-      data = inp.data
-
-      # Optional use of output_info
-      inference_params = {}
-      if "params" in output_info:
-        inference_params = output_info["params"]
-
-      temperature = inference_params.get("temperature", 0.7)
-      max_tokens = inference_params.get("max_tokens", 256)
-      top_p = inference_params.get("top_p", .9)
-
-      if data.text.raw != "":
-        prompt = data.text.raw
-        messages = [{"role": "user", "content": prompt}]
-        gen_config = GenerationConfig(temperature=temperature,
-                                      max_new_tokens=max_tokens,
-                                      top_p=top_p)
-        prompt = self.tokenizer.apply_chat_template(messages, tokenize=False)
-        res = self.pipe(prompt,
-                   gen_config=gen_config)
-        text = res.text.replace("<|start_header_id|>assistant<|end_header_id|>", "")
-        text = text.replace("\n\n", "")
-        output.data.text.raw = text
-
-      output.status.code = status_code_pb2.SUCCESS
-      outputs.append(output)
-    return service_pb2.MultiOutputResponse(outputs=outputs,)
+    return service_pb2.MultiOutputResponse(outputs=outputs)
 
   def generate(self, request: service_pb2.PostModelOutputsRequest
               ) -> Iterator[service_pb2.MultiOutputResponse]:
     """Example yielding a whole batch of streamed stuff back."""
 
-    # TODO: Could cache the model and this conversion if the hash is the same.
-    model = request.model
-    output_info = None
-    if request.model.model_version.id != "":
-      output_info = json_format.MessageToDict(
-          model.model_version.output_info, preserving_proto_field_name=True)
+    prompts = [inp.data.text.raw for inp in request.inputs]
+    inference_params = get_inference_params(request)
+    temperature = inference_params.get("temperature", 0.7)
+    max_tokens = inference_params.get("max_tokens", 256)
+    top_p = inference_params.get("top_p", .9)
+    
+    messages = []
+    for prompt in prompts:
+      try:
+        prompt = json.loads(prompt)
+      except:
+        prompt = [{"role": "user", "content": prompt}]
+      finally:
+        messages.append(prompt)
+    
+    messages = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    gen_config = GenerationConfig(temperature=temperature,
+                                  max_new_tokens=max_tokens,
+                                  top_p=top_p)
+    
+    batch_size = len(prompts)
+    outputs = [
+        resources_pb2.Output(
+          data=resources_pb2.Data(text=resources_pb2.Text(raw="")),
+          status=status_pb2.Status(code=status_code_pb2.SUCCESS)
+        ) for _ in range(batch_size)
+      ]
+    self.pipe.running_session_ids
 
-    # TODO: Could cache the model and this conversion if the hash is the same.
-    model = request.model
-    output_info = None
-    if request.model.model_version.id != "":
-      output_info = json_format.MessageToDict(
-          model.model_version.output_info, preserving_proto_field_name=True)
-
-    # TODO: parallelize this over inputs in a single request.
-    for inp in request.inputs:
-      output = resources_pb2.Output()
-
-      data = inp.data
-
-      # Optional use of output_info
-      inference_params = {}
-      if "params" in output_info:
-        inference_params = output_info["params"]
-
-      messages = []
-      temperature = inference_params.get("temperature", 0.7)
-      max_tokens = inference_params.get("max_tokens", 256)
-      top_p = inference_params.get("top_p", .9)
-
-      if data.text.raw != "":
-        prompt = data.text.raw
-        messages.append({"role": "user", "content": prompt})
-        kwargs = dict(
-            temperature=temperature,
-            max_new_tokens=max_tokens,
-            top_p=top_p,
-        )
-        gen_config = GenerationConfig(**kwargs)
-        prompt = self.tokenizer.apply_chat_template(messages, tokenize=False)
-        for item in self.pipe.stream_infer(prompt, gen_config=gen_config):
-          text = item.text.replace(
-              "<|start_header_id|>assistant<|end_header_id|>", "")
-          text = text.replace("\n\n", "")
-          output.data.text.raw = text
-          output.status.code = status_code_pb2.SUCCESS
-          yield service_pb2.MultiOutputResponse(outputs=[output],)
+    current_session_id = next(deepcopy(self.pipe._session_id))
+    for item in self.pipe.stream_infer(messages, gen_config=gen_config):
+      text = item.text
+      running_idx = item.session_id
+      idx = running_idx - current_session_id
+      outputs[idx].data.text.raw = text
+      yield service_pb2.MultiOutputResponse(outputs=outputs,)
         
 
   def stream(self, request_iterator: Iterator[service_pb2.PostModelOutputsRequest]
             ) -> Iterator[service_pb2.MultiOutputResponse]:
-    """Example yielding a whole batch of streamed stuff back."""
-
-    for ri, request in enumerate(request_iterator):
-      output_info = None
-      if ri == 0:  # only first request has model information.
-        model = request.model
-        if request.model.model_version.id != "":
-          output_info = json_format.MessageToDict(
-              model.model_version.output_info, preserving_proto_field_name=True)
-          # Optional use of output_info
-          inference_params = {}
-          if "params" in output_info:
-            inference_params = output_info["params"]
-      # TODO: parallelize this over inputs in a single request.
-      for inp in request.inputs:
-        output = resources_pb2.Output()
-
-        data = inp.data
-
-        system_prompt = "You are a helpful assistant"
-
-        messages = [{"role": "system", "content": system_prompt}]
-        temperature = inference_params.get("temperature", 0.7)
-        max_tokens = inference_params.get("max_tokens", 100)
-        top_p = inference_params.get("top_p", 1.0)
-
-        if data.text.raw != "":
-          prompt = data.text.raw
-          messages.append({"role": "user", "content": prompt})
-          kwargs = dict(
-              model=self.model,
-              messages=messages,
-              temperature=temperature,
-              max_tokens=max_tokens,
-              top_p=top_p,
-              stream=True,
-          )
-          stream = self.client.chat.completions.create(**kwargs)
-          for chunk in stream:
-            if chunk.choices[0].delta.content is None:
-              continue
-            output.data.text.raw = chunk.choices[0].delta.content
-            output.status.code = status_code_pb2.SUCCESS
-            yield service_pb2.MultiOutputResponse(outputs=[output],)
+    pass
