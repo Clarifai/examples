e4b99748f52d8692a90a52e2fdc42ef7459457a0
David Eigen <deigen@clarifai.com>
2025-01-24 16:22:39 -0500

    update string cat example

--- models/model_upload/test-upload/python_string_cat/1/model.py
+++ models/model_upload/test-upload/python_string_cat/1/model.py
@@ -1,16 +1,18 @@
 import hashlib
 from typing import Iterator
 
-from clarifai.runners.models.model_runner import ModelRunner
+from clarifai.runners.models.model_class import ModelClass
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2
 from clarifai_grpc.grpc.api.status import status_code_pb2
 from google.protobuf import json_format
 
 
-class MyRunner(ModelRunner):
+class PythonStringCat(ModelClass):
   """A custom runner that adds "Hello World" to the end of the text and replaces the domain of the
   image URL as an example.
   """
+  def __init__(self, prefix='Hello World'):
+    self.prefix = prefix
 
   def load_model(self):
     """Load the model here."""
@@ -40,16 +42,8 @@ class MyRunner(ModelRunner):
       if "params" in output_info:
         params_dict = output_info["params"]
 
-      if data.text.raw != "":
-        output.data.text.raw = data.text.raw + "Hello World" + params_dict.get("hello", "")
-      if data.image.url != "":
-        output.data.text.raw = data.image.url.replace(
-            "samples.clarifai.com",
-            "newdomain.com" + params_dict.get("domain",),
-        )
-      if data.image.base64 != b"":
-        output.data.image.base64 = (
-            hashlib.md5(data.image.base64).hexdigest() + "Hello World run_input")
+      output.data.text.raw = self.prefix + " " + data.text.raw + " " + params_dict.get("testparam", "")
+
       output.status.code = status_code_pb2.SUCCESS
       outputs.append(output)
     return service_pb2.MultiOutputResponse(outputs=outputs,)
@@ -58,22 +52,11 @@ class MyRunner(ModelRunner):
               ) -> Iterator[service_pb2.MultiOutputResponse]:
     """Example yielding a whole batch of streamed stuff back."""
 
-    # model = request.model
-    # output_info = None
-    # if request.model.model_version.id != "":
-    #   output_info = json_format.MessageToDict(
-    #     model.model_version.output_info, preserving_proto_field_name=True
-    #   )
-
-    for i in range(10):  # fake something iterating generating 10 times.
+    for i in range(10):  # Generate 10 outputs.
       outputs = []
       for inp in request.inputs:
         output = resources_pb2.Output()
-        if inp.data.text.raw != "":
-          output.data.text.raw = f"Generate Hello World {i}"
-        if inp.data.image.base64 != b"":
-          output.data.text.raw = (
-              hashlib.md5(inp.data.image.base64).hexdigest() + f"Generate Hello World {i}")
+        output.data.text.raw = self.prefix + " " + inp.data.text.raw + f"generate {i}"
         output.status.code = status_code_pb2.SUCCESS
         outputs.append(output)
       yield service_pb2.MultiOutputResponse(outputs=outputs,)
@@ -82,24 +65,11 @@ class MyRunner(ModelRunner):
             ) -> Iterator[service_pb2.MultiOutputResponse]:
     """Example yielding a whole batch of streamed stuff back."""
 
-    for ri, request in enumerate(request_iterator):
-      # if ri == 0: # only first request has model information.
-      #   model = request.model
-      #   output_info = None
-      #   if request.model.model_version.id != "":
-      #     output_info = json_format.MessageToDict(
-      #       model.model_version.output_info, preserving_proto_field_name=True
-      #     )
-      for i in range(10):  # fake something iterating generating 10 times.
-        outputs = []
-        for inp in request.inputs:
-          output = resources_pb2.Output()
-          if inp.data.text.raw != "":
-            out_text = inp.data.text.raw + f"Stream Hello World {i}"
-          if inp.data.image.base64 != b"":
-            out_text = hashlib.md5(inp.data.image.base64).hexdigest() + f"Stream Hello World {i}"
-          output.status.code = status_code_pb2.SUCCESS
-          print(out_text)
-          output.data.text.raw = out_text
-          outputs.append(output)
-        yield service_pb2.MultiOutputResponse(outputs=outputs,)
+    for i, request in enumerate(request_iterator):
+       outputs = []
+       for inp in request.inputs:
+         output = resources_pb2.Output()
+         output.data.text.raw = self.prefix + " " + inp.data.text.raw + f"stream {i}"
+         output.status.code = status_code_pb2.SUCCESS
+         outputs.append(output)
+       yield service_pb2.MultiOutputResponse(outputs=outputs,)
