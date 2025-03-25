e3a2782d6c57fd45456bd549b0a4fe794467f7ac
luv-bansal <bansal22luvi@gmail.com>
2025-03-25 04:48:53 -0400

    Pythonic String cat Model

---
+++ models/model_upload/test-upload/pythonic_string_cat/1/model.py
@@ -0,0 +1,35 @@
+from clarifai.runners.models.model_class import ModelClass
+from clarifai.runners.utils.data_types import Stream
+
+
+class PythonicStringCat(ModelClass):
+  """A custom runner that adds "Hello World" to the end of the text."""
+
+  def load_model(self):
+    """Load the model here."""
+
+  @ModelClass.method
+  def predict(self, text1: str = "") -> str:
+    """This is the method that will be called when the runner is run. It takes in an input and
+    returns an output.
+    """
+
+    output_text = text1 + "Hello World"
+
+    return output_text
+
+  @ModelClass.method
+  def generate(self, text1: str = '') -> Stream[str]:
+    """Example yielding a whole batch of streamed stuff back."""
+
+    for i in range(10):  # fake something iterating generating 10 times.
+      output_text = text1 + f"Generate Hello World {i}"
+      yield output_text
+
+  @ModelClass.method
+  def stream(self, input_iterator: Stream[str]) -> Stream[str]:
+    """Example yielding a whole batch of streamed stuff back."""
+
+    for i, input in enumerate(input_iterator):
+      output_text = input + f"Stream Hello World {i}"
+      yield output_text
---
+++ models/model_upload/test-upload/pythonic_string_cat/1/test_model.py
@@ -0,0 +1,23 @@
+# Runner test your implementation locally
+#
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+from model import PythonicStringCat
+
+if __name__ == "__main__":
+  model = PythonicStringCat()
+  model.load_model()
+  prompt = "Write 2000 word story?"
+
+  res = model.predict(prompt)
+  assert res == "Write 2000 word story?Hello World", 'predict failed'
+
+  res = model.generate(prompt)
+  for i, r in enumerate(res):
+    assert r == f"Write 2000 word story?Generate Hello World {i}", 'generate failed'
+
+  res = model.stream(iter([prompt] * 5))
+  for i, r in enumerate(res):
+    assert r == f"Write 2000 word story?Stream Hello World {i}", 'stream failed'
